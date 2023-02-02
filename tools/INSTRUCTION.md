51c616a444620d8bc04e5c5d0d6f4062d921b3a8
iamzielus <57290500+iamzielus@users.noreply.github.com>
2023-02-02 23:28:42 +0100

    tools update

--- tools/INSTRUCTION.md
+++ tools/INSTRUCTION.md
@@ -5,5 +5,6 @@
 # Bruteforce Caesar Cipher
 In order to use this tool run this command:
 
-	python3 brute_caesar.py
-	
+	python3 brute_caesar.py charTable startPoint endPoint encodedString
+
+Do not change arguments order. For ASCII table use "0" as a "charTable" argument.	
--- tools/brute_caesar.py
+++ tools/brute_caesar.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python3
 #This Python script allows bruteforcing Caesar Cipher
 #Author: github/iamzielus
-#Ask for a string to decode
-encodedString = input("What text to decode?")
-print("============================")
+
+import sys
+
+#Command execution: python3 filename charTable startPoint endPoint encodedString
+#Note that starting point should be the code of the first char in range, not the one before
+#Remember to use backslash before special characters
+#For default ASCII table use "0" as an option
+
+charTable = []
+#Assign character table
+if sys.argv[1] != "0":
+    for x in sys.argv[1]:
+       charTable.append(x)
+else:
+    charTable = [chr(n) for n in range(128)]
+
+startPoint = int(sys.argv[2])
+endPoint = int(sys.argv[3])
+encodedString = sys.argv[4]
 
 #Looping through shifts
-for i in range(94):
+for i in range(endPoint-startPoint):
     decodedString = str(i)+": "
     for letter in encodedString:
-        #This "if" statement keeps decoded characters in range 32-126
-        if ord(letter)+i > 126 :
-            decodedString += chr(31+(ord(letter)+i-125))
-        else:
-            decodedString += chr(ord(letter)+i)
+        if letter in charTable:
+            #This "if" statement keeps decoded characters in range
+            if charTable.index(letter) + i > endPoint :
+                decodedString += charTable[startPoint+(charTable.index(letter)+i-endPoint)]
+            else:
+                decodedString += charTable[charTable.index(letter)+i]
     #Print decoded string
     print(decodedString)
